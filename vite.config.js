8768dd1f0b0cbae6b26265b3a6fe600f686f037d
Kamil Cieśla <kamilciesla34@gmail.com>
2023-04-08 16:39:31 +0200

    setup

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+// from https://blog.reactprocoder.com/how-to-setup-pwaprogressive-web-app-in-vite-react-app
+// to merge with code above later
+// import { createPWA } from 'vite-plugin-pwa'
+
+// const pwaPlugin = createPWA({
+//   config: require('./pwa.config.js'),
+// })
+
+// export default {
+//   // Your other Vite config options...
+//   plugins: [pwaPlugin],
+// }
