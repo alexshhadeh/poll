a1c6283f88dc5cd89318975cd1bd1d52cd5a9b80
Kamil Cieśla <kamil.ciesla@zendesk.com>
2023-04-13 20:15:23 +0200

    create folder structure

---
+++ src/api/poll/poll.js
@@ -0,0 +1,26 @@
+class Poll {
+
+    create(title, fields, allow_multiselect) {
+
+    }
+
+    delete() {
+
+    }
+
+    get link() {
+
+    }
+
+    get results() {
+
+    }
+
+    get poll() {
+
+    }
+
+    vote() {
+
+    }
+}
