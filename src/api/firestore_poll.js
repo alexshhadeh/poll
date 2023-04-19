40ab37f10cf4e6216ab3fc5d8117ab7486114b4d
chrisfid <krzysztof.fidyka.dev@gmail.com>
2023-04-19 12:16:13 +0200

    fix: voting issue

--- src/api/firestore_poll.js
+++ src/api/firestore_poll.js
@@ -1,34 +1,37 @@
-import db from './firestore_db'
+import db from './firestore_db';
 
-import { collection, addDoc, doc, getDoc, updateDoc } from 'firebase/firestore'
+import { collection, addDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
 
 export async function createPoll(pollDocument) {
-    const pollReference = await addDoc(collection(db, "polls"), pollDocument);
-    console.log(`%cPoll created in database with ID: ${pollReference.id}`, "color: green;");
-    return pollReference.id;
+  const pollReference = await addDoc(collection(db, 'polls'), pollDocument);
+  console.log(
+    `%cPoll created in database with ID: ${pollReference.id}`,
+    'color: green;'
+  );
+  return pollReference.id;
 }
 
 export async function getPoll(pollId) {
-    const pollRef = doc(db, "polls", pollId);
-    const pollDoc = await getDoc(pollRef);
-    console.log(`%cFetched poll with Id: ${pollId}`, "color: green;");
-    return pollDoc.data();
+  const pollRef = doc(db, 'polls', pollId);
+  const pollDoc = await getDoc(pollRef);
+  console.log(`%cFetched poll with Id: ${pollId}`, 'color: green;');
+  return pollDoc.data();
 }
 
 export async function sendVotes(pollId, choices) {
-    const poll = await getPoll(pollId);
-    const updatedFields = poll.fields;
-    console.log(updatedFields)
-    console.log(choices);
-    for (const choice in choices) {
-        updatedFields[String(choice)] += choices[String(choice)]
-    }
+  const poll = await getPoll(pollId);
+  const updatedFields = { ...poll.fields };
+  console.log(updatedFields);
+  console.log(choices);
+  for (const choice in choices) {
+    updatedFields[choice] += choices[choice];
+  }
 
-    const pollRef = doc(db, "polls", pollId);
+  const pollRef = doc(db, 'polls', pollId);
 
-    await updateDoc(pollRef, {
-        fields: updatedFields
-    });
+  await updateDoc(pollRef, {
+    fields: updatedFields,
+  });
 
-    console.log(`%cSent votes to poll with Id: ${pollId}`, "color: green;");
+  console.log(`%cSent votes to poll with Id: ${pollId}`, 'color: green;');
 }
--- src/poll/poll.js
+++ src/poll/poll.js
@@ -1,61 +1,59 @@
-import * as firestore from '../api/firestore_poll'
+import * as firestore from '../api/firestore_poll';
 
 export class Poll {
+  static create(userId, title, fields, allow_multiselect) {
+    console.log('user id is ' + userId);
+    const params = {
+      user_id: userId,
+      title: title,
+      fields: fields.reduce((acc, curr) => {
+        acc[curr] = 0;
+        return acc;
+      }, {}),
 
-    static create(userId, title, fields, allow_multiselect) {
-        console.log('user id is ' + userId)
-        const params = {
-            user_id: userId,
-            title: title,
-            fields: fields.reduce((acc, curr) => {
-                acc[curr] = 0;
-                return acc;
-            }, {}),
+      allow_multiselect: allow_multiselect,
+    };
 
-            allow_multiselect: allow_multiselect
-        }
-
-        if (Poll.verifyParams(params)) {
-            console.log('params that are sent to db')
-            console.log(params)
-            return firestore.createPoll(params)
-        }
+    if (Poll.verifyParams(params)) {
+      console.log('params that are sent to db');
+      console.log(params);
+      return firestore.createPoll(params);
     }
+  }
 
-    static async results(pollId) {
-        const poll = await firestore.getPoll(pollId);
-        return poll.fields
+  static async results(pollId) {
+    const poll = await firestore.getPoll(pollId);
+    return poll.fields;
+  }
+
+  static async poll(pollId) {
+    const poll = await firestore.getPoll(pollId);
+    //Return only field names to hide current vote results
+    poll.fields = Object.keys(poll.fields);
+    return poll;
+  }
+
+  static async vote(pollId, choices) {
+    console.log('Sending vote choices');
+    if (this.verifyVotingChoices(choices)) {
+      await firestore.sendVotes(pollId, choices);
     }
+  }
 
-    static async poll(pollId) {
-        const poll = await firestore.getPoll(pollId);
-        //Return only field names to hide current vote results
-        poll.fields = Object.keys(poll.fields)
-        return poll;
+  static verifyParams(pollObject) {
+    for (const key in pollObject) {
+      if (pollObject[key] == null) {
+        throw new Error(`Poll has null value on "${key}" key`);
+      }
     }
+    return true;
+  }
 
-    static async vote(pollId, choices) {
-        console.log("Sending vote choices")
-        if (this.verifyVotingChoices(choices)) {
-            await firestore.sendVotes(pollId, choices)
-        }
+  static verifyVotingChoices(choices) {
+    if (choices && Object.keys(choices).length) {
+      return true;
+    } else {
+      throw new Error('No choices were provided!');
     }
-
-    static verifyParams(pollObject) {
-        for (const key in pollObject) {
-            if (pollObject[key] == null) {
-                throw new Error(`Poll has null value on "${key}" key`)
-            }
-        }
-        return true;
-    }
-
-    static verifyVotingChoices(choices) {
-        if (choices && Object.keys(choices).length) {
-            return true;
-        } else {
-            throw new Error('No choices were provided!')
-        }
-    }
-
+  }
 }
